cddbbb4b81f3677edac7e22eb8aa8f04cecbb57b
ludacirs <ludacirs@naver.com>
2022-02-12 15:58:25 +0900

    fix: api 주소 수정

--- apis/rating.ts
+++ apis/rating.ts
@@ -1,6 +1,6 @@
 export const getRating = async () => {
   const response = await fetch(
-    `https://dsmd.kr:8093/rating?agency=%ED%95%9C%EA%B5%AD%EA%B0%A4%EB%9F%BD%EC%A1%B0%EC%82%AC%EC%97%B0%EA%B5%AC%EC%86%8C`,
+    `${process.env.NEXT_PUBLIC_API}/rating?agency=%ED%95%9C%EA%B5%AD%EA%B0%A4%EB%9F%BD%EC%A1%B0%EC%82%AC%EC%97%B0%EA%B5%AC%EC%86%8C`,
   );
   return await response.json();
 };
