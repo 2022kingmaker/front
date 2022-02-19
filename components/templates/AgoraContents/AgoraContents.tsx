628ecad7a8132b867931f68dc6cabe6b3eb33e84
ludacirs <ludacirs@naver.com>
2022-02-20 01:33:03 +0900

    feat: 닉네임 위치 변경

--- components/templates/AgoraContents/AgoraContents.tsx
+++ components/templates/AgoraContents/AgoraContents.tsx
@@ -60,7 +60,9 @@ const UserTab = styled.section`
 const Writer = styled.div`
   margin-top: 5px;
   color: #333;
-  font-size: 8px;
+  font-size: 12px;
+  line-height: 1.3;
+  word-break: break-word;
 `;
 
 interface AgoraContentsProps {}
@@ -75,27 +77,28 @@ const AgoraContents = ({}: AgoraContentsProps) => {
       <TalkContainer>
         <UserTab>
           <Avatar writer={'섬뜩한 '} backgroundColor={'#1F4D9C'} />
-          <Writer>섬뜩한 고양이</Writer>
+          <Writer>{makeNewLine('섬뜩한 고양이')}</Writer>
         </UserTab>
         <TalkBubble color={'#1F4D9C'}></TalkBubble>
       </TalkContainer>
       <TalkContainer>
         <UserTab>
           <Avatar writer={'고양이'} backgroundColor={'#D33736'} />
-          <Writer>고양이</Writer>
+          <Writer>{makeNewLine('섬뜩한 고양이')}</Writer>
         </UserTab>
         <TalkBubble color={'#D33736'}></TalkBubble>
       </TalkContainer>
       <TalkContainer>
         <UserTab>
           <Avatar writer={'고양이'} backgroundColor={'#F7CE46'} />
+          <Writer>{makeNewLine('섬뜩한 고양이')}</Writer>
         </UserTab>
         <TalkBubble color={'#F7CE46'}></TalkBubble>
       </TalkContainer>
       <TalkContainer>
         <UserTab>
           <Avatar writer={'고양이'} backgroundColor={'#D95F29'} />
-          <Writer>고양이</Writer>
+          <Writer>{makeNewLine('섬뜩한 고양이')}</Writer>
         </UserTab>
         <TalkBubble color={'#D95F29'}></TalkBubble>
       </TalkContainer>
@@ -109,7 +112,12 @@ const AgoraContents = ({}: AgoraContentsProps) => {
 };
 
 export default AgoraContents;
-//    first: '#1F4D9C',
-//     second: '#D33736',
-//     third: '#F7CE46',
-//     fourth: '#D95F29',
+
+//['섬뜩한','고양이']
+const makeNewLine = (nickname: string) =>
+  nickname.split(' ').map(word => (
+    <>
+      <span>{word}</span>
+      <br />
+    </>
+  ));
