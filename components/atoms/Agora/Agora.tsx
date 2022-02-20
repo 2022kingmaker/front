cbdbbcbdccbf7e25f276f4b2c63cb2ac55486f9a
ludacirs <ludacirs@naver.com>
2022-02-20 19:08:37 +0900

    feat: fixed Agora 기능

--- components/atoms/Agora/Agora.tsx
+++ components/atoms/Agora/Agora.tsx
@@ -3,7 +3,13 @@ import { Talk } from '@models/Agora';
 import { flexBox } from '@styles/mixin';
 import Link from 'next/link';
 
-const AgoraBlock = styled.a`
+const Test = styled.div`
+  ${flexBox()};
+  width: 100%;
+  position: relative;
+`;
+
+const AgoraBlock = styled.a<{ fixed: boolean }>`
   ${flexBox('space-around', 'flex-start', 'column')};
   padding: 12px;
   width: 100%;
@@ -15,6 +21,25 @@ const AgoraBlock = styled.a`
   .text {
     color: gray;
     font-size: 14px;
+  }
+
+  ${({ fixed }) =>
+    fixed
+      ? `
+  position: fixed;
+  margin: 0 auto; 
+  left: 200px;
+  right: 0;
+  top: 120px;
+  width: 75%;
+  z-index:2;
+  `
+      : ''};
+
+  @media ${({ theme }) => theme.desktop} {
+    width: 90%;
+    left: 0;
+    right: 0;
   }
 `;
 
@@ -42,28 +67,32 @@ interface AgoraProps {
   agenda: string;
   description: string;
   talks?: Talk[];
+  fixed: boolean;
 }
 
 const Agora = ({
   agenda = '일자리 창출 이런게 필요해요~',
   description = '모든 후보가 공격적인 일자리 창출 공약을 내걸고 있는데요. 여러분의 생각은 어떠신가요?',
   talks,
+  fixed,
 }: AgoraProps) => {
   return (
-    <Link href={'/agora/1'} passHref>
-      <AgoraBlock>
-        <Title>{agenda}</Title>
-        <Description>{description}</Description>
-        {talks ? (
-          <Info>
-            <Parties>ㅁ ㅁ ㅁ ㅁ </Parties>
-            <UpdateTime>3분 전</UpdateTime>
-          </Info>
-        ) : (
-          <div className={'text'}>자유로운 의견 나눠주세요!</div>
-        )}
-      </AgoraBlock>
-    </Link>
+    <Test>
+      <Link href={'/agora/1'} passHref>
+        <AgoraBlock fixed={fixed}>
+          <Title>{agenda}</Title>
+          <Description>{description}</Description>
+          {talks ? (
+            <Info>
+              <Parties>ㅁ ㅁ ㅁ ㅁ </Parties>
+              <UpdateTime>3분 전</UpdateTime>
+            </Info>
+          ) : (
+            <div className={'text'}>자유로운 의견 나눠주세요!</div>
+          )}
+        </AgoraBlock>
+      </Link>
+    </Test>
   );
 };
 
